dd568780f17ff45b472702e85314dbd10c142d5f
ssrathi05 <rathishreya2005@gmail.com>
2025-08-10 20:54:44 -0400

    added openAI key

--- project6/main.py
+++ project6/main.py
@@ -1,2 +1,9 @@
-def hi():
-  print("hi")
+import cv2
+import numpy as np
+import streamlit as st
+from tensorflow.keras.applications.mobilenet_v2 import (
+    MobileNetV2,
+    preprocess_input,
+    decode_predictions
+)
+from PIL import Image
